bf46b70ae4c375f1a7ca5890a47d7b42aef0fb22
Silas L. Pascoal <slpascoal01@gmail.com>
2022-12-08 10:51:17 -0300

    Resumos e Códigos: JavaScript - Básico

---
+++ JavaScript_Básico/app.js
@@ -0,0 +1,35 @@
+// Todos os conceitos de Javacript - Básico serão aplicados aqui 
+
+//criação de variavel let
+let mensagem = "Olá"
+mensagem = "Olá"
+
+//criação de constante
+const pi = 3.14
+
+//atribuições e incrementos
+let num1 = 1
+let num2 = 3
+num1++
+num2--
+
+//operações aritméticas
+const resultado = num1 / num2
+
+//objeto Math
+let aleatorio = Math.round(Math.random() * (100 - 0) + 0)
+
+//strings
+let minhaString = "Ola Mundo"
+
+//array
+const alunos = ['ana', 'maria']
+
+alunos.push('silas')
+
+//Console.log para exibir os resultados
+console.log(`${mensagem}, o número PI é= ${pi}`)
+console.log(resultado)
+console.log(`as chances de chover hoje são de: ${aleatorio}%`)
+console.log(minhaString.match('a'))
+console.log(alunos)
