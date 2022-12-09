54c696c159708565b22fa59f241d5af26591b06d
Silas L. Pascoal <slpascoal01@gmail.com>
2022-12-09 10:15:44 -0300

    Funções e Objetos ao JavaScript - Básico

--- JavaScript_Básico/app.js
+++ JavaScript_Básico/app.js
@@ -27,9 +27,50 @@ const alunos = ['ana', 'maria']
 
 alunos.push('silas')
 
+//funções
+function somar(num1, num2) {
+    return num1 + num2
+}
+
+let minhaSoma = somar(2, 3)
+
+//objeto
+function criaPessoa(nome, idade) {
+    return{ 
+        nome: nome,
+        idade: idade,
+        
+        fala() {
+            console.log(`Sou uma pessoa, meu nome é ${this.nome} e tenho ${this.idade} anos`)
+            console.log('*')
+        }
+    };
+}
+
+const pessoa1 = criaPessoa('silas', 21)
+const pessoa2 = criaPessoa('maria', 20)
+
 //Console.log para exibir os resultados
+console.log('*Variaveis, console.log e constantes')
 console.log(`${mensagem}, o número PI é= ${pi}`)
+console.log('-----------------')
+console.log('*operações aritméticas')
 console.log(resultado)
+console.log('-----------------')
+console.log('*objeto math')
 console.log(`as chances de chover hoje são de: ${aleatorio}%`)
+console.log('-----------------')
+console.log('*strings')
 console.log(minhaString.match('a'))
+console.log('-----------------')
+console.log('*array')
 console.log(alunos)
+console.log('-----------------')
+console.log('*funções')
+console.log(minhaSoma)
+console.log('-----------------')
+console.log('*objeto')
+console.log(pessoa1.fala())
+console.log('**')
+console.log(pessoa2.fala())
+console.log('-----------------')
